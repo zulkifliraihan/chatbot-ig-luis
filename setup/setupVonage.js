b700717c258947d7ab85f606a8539eba4ce8f2b9
Zulkifli Raihan <zuran2907@gmail.com>
2022-06-22 03:44:01 +0700

    Prepare for Real Facebook

--- setup/setupVonage.js
+++ setup/setupVonage.js
@@ -1,16 +1,30 @@
+require('dotenv').config();
+
 const Vonage = require('@vonage/server-sdk');
 
 class setupVonage {
     constructor(env) {
     
-        this.credentialsVonage = new Vonage({
-            apiKey: env.VONAGE_API_KEY,
-            apiSecret: env.VONAGE_API_SECRET,
-            applicationId: env.VONAGE_APPLICATION_ID,
-            privateKey: env.VONAGE_APPLICATION_PRIVATE_KEY_PATH
-        }, {
-            apiHost: 'https://messages-sandbox.nexmo.com'
-        });
+        if (process.env.APPENV == "production") {
+
+            this.credentialsVonage = new Vonage({
+                apiKey: env.VONAGE_API_KEY,
+                apiSecret: env.VONAGE_API_SECRET,
+                applicationId: env.VONAGE_APPLICATION_ID,
+                privateKey: env.VONAGE_APPLICATION_PRIVATE_KEY_PATH
+            });
+            
+        } else {
+            
+            this.credentialsVonage = new Vonage({
+                apiKey: env.VONAGE_API_KEY,
+                apiSecret: env.VONAGE_API_SECRET,
+                applicationId: env.VONAGE_APPLICATION_ID,
+                privateKey: env.VONAGE_APPLICATION_PRIVATE_KEY_PATH
+            }, {
+                apiHost: 'https://messages-sandbox.nexmo.com'
+            });
+        }
 
     }
 
